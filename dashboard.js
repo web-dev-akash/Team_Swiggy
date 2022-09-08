dc39b0d6576bac409c47cbf94157c7dfd8bcc47c
web-dev-akash <akumar415129.ak@gmail.com>
2022-09-08 13:26:51 +0530

    Resources Images

--- dashboard.js
+++ dashboard.js
@@ -1,60 +1,51 @@
 function getData() {
   fetch(`https://swiggy-clone-akash.herokuapp.com/data`).then((response) =>
-    response
-      .json()
-      .then((data) => ({
-        data: data,
-        status: response.status,
-      }))
-      .then((res) => {
-        const data = res.data;
-        renderDom(data);
-      })
+    response.json().then((res) => {
+      // const data = res.data;
+      // console.log(res[0]);
+      // console.log(data);
+      renderDom(res);
+    })
   );
 }
 getData();
 
 function renderDom(data) {
-  console.log(data);
-  let target = document.getElementById("restaurants_cards");
-  let count_target = document.getElementById("total_restaurant_count");
-  let row = document.createElement("div");
-  row.setAttribute("class", "row py-4");
+  // console.log(data);
+  let container = document.getElementById("ak-restaurant-data");
+  let count_target = document.getElementById("ak-restaurant-count");
   if (data != []) {
     count_target.innerText = data.length + " Restaurants";
     for (let i = 0; i < data.length; i++) {
-      let rating = Number(data[i].aggregate_rating);
+      let rating = Number(data[i].AggregateRating);
       let color_code;
       if (rating > 3.5) {
-        color_code = "badge-success";
+        color_code = "green";
       } else if (rating < 3.5 && rating > 2.3) {
         color_code = "badge-warning";
       } else {
         color_code = "badge-danger";
       }
-      row.innerHTML += ` 
-            <a href="user_restaurent_view.html?restaurant_id=${data[i].restaurant_id}"><div class="col-3 mb-4">
-                    <div class="card">
-                        <img src="Resource/${data[i].restaurant_images}" style="width: 245px ; height: 200px" class="card-img-top" alt="restaurant_img">
-                    <div class="card-body">
+      container.innerHTML += ` 
+            <a href="eg_user_restaurent_view.html?restaurantID=${data[i].RestaurantID}">
+                    <div>
+                        <img src="${data[i].restaurantImages}" alt="restaurant_img">
+                    <div>
 
-                        <p>${data[i].restaurant_name}</p>
-                        <p class="text-muted">${data[i].Cuisines}</p>
-                        <div class="col">
-                            <span class="badge ${color_code}">★ ${rating}</span>
+                        <p>${data[i].RestaurantName}</p>
+                        <p>${data[i].Cuisines}</p>
+                        <div>
+                            <span>★ ${rating}</span>
                         </div>
                         
-                        <div class="col">
-                            <p>₹${data[i].average_Cost_for_two} FOR TWO</p>
+                        <div>
+                            <p>₹ ${data[i].AverageCost} FOR TWO</p>
                         </div>
-                        <div class="dropdown-divider"></div>
-                        <a href="">Look Up the restaurant</a>
-
                     </div>
                 </div>
-            </div></a>`;
+            </div>
+            </a>`;
     }
-    target.append(row);
   } else {
     target.innerHTML = `<p class="h3">No Result Found</p>`;
   }
